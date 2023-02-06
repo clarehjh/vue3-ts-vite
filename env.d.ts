a1d6add362320becb12a0e89bbc807e4ab5ab057
huangjiahui <>
2023-02-06 17:07:00 +0800

    '当前提交的相关描述'

---
+++ env.d.ts
@@ -0,0 +1,15 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: Huangjiahui
+ * @Date: 2023-02-06 16:11:28
+ * @LastEditors: Huangjiahui
+ * @LastEditTime: 2023-02-06 16:36:25
+ */
+/// <reference types="vite/client" />
+declare module "*.vue" {
+  import { DefineComponent } from "vue";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
