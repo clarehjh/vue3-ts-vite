b75b444214952349e6df998660cd2eabf128b7ce
huangjiahui <>
2023-03-03 18:16:39 +0800

    '主页面包屑搭建'

---
+++ src/components/main-header/main-header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main-header">
+    <div class="menu-icon" @click="handleMenuIconClick">
+      <el-icon size="20px">
+        <component :is="isFold ? 'Expand' : 'Fold'"></component>
+      </el-icon>
+    </div>
+    <div class="content">
+      <div class="breadcrumb">面包屑</div>
+      <header-info />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import HeaderInfo from './cpns/header-info'
+
+// 0.内部自定义事件
+const emit = defineEmits(['foldChange'])
+
+// 1.记录状态
+const isFold = ref(false)
+function handleMenuIconClick() {
+  // 1.内部改变状态
+  isFold.value = !isFold.value
+
+  // 2.将事件和状态传递给父组件
+  emit('foldChange', isFold.value)
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -4,15 +4,57 @@
  * @Author: Huangjiahui
  * @Date: 2023-02-07 11:08:25
  * @LastEditors: Huangjiahui
- * @LastEditTime: 2023-02-09 09:55:40
+ * @LastEditTime: 2023-03-03 18:06:53
 -->
 <template>
-  <div class="main">main</div>
+  <div class="main">
+    <el-container class="main-content">
+      <el-aside :width="isFold ? '60px' : '210px'">
+        <!-- <main-menu :is-fold="isFold"></main-menu> -->
+      </el-aside>
+      <el-container>
+        <el-header height="50px">
+          <main-header @fold-change="handleChang"></main-header>
+        </el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script setup>
 import { reactive } from 'vue'
 </script>
 
-<style lang="less" scoped></style>
-                 
+<style lang="less" scoped>
+.main {
+  height: 100%;
+}
+
+.main-content {
+  height: 100%;
+
+  .el-aside {
+    overflow-x: hidden;
+    overflow-y: auto;
+    line-height: 200px;
+    text-align: left;
+    cursor: pointer;
+    background-color: #001529;
+    scrollbar-width: none; /* firefox */
+    -ms-overflow-style: none; /* IE 10+ */
+
+    transition: width 0.3s ease;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  .el-main {
+    background-color: #f0f2f5;
+  }
+}
+</style>
